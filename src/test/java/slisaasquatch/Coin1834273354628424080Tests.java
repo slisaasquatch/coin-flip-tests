bc76ed921322b7b6954d961b8505b9aff3d9a280
slisaasquatch <sli@saasquat.ch>
2024-09-15 18:07:43 -0700

    Update Coin1834273354628424080Tests.java

--- src/test/java/slisaasquatch/Coin1834273354628424080Tests.java
+++ src/test/java/slisaasquatch/Coin1834273354628424080Tests.java
@@ -17,6 +17,8 @@ public class Coin1834273354628424080Tests {
     assertEquals(2, aliceAlgorithm(new boolean[]{true, true}));
     assertEquals(4, aliceAlgorithm(new boolean[]{true, false, false, true}));
     assertEquals(3, aliceAlgorithm(new boolean[]{true, false, true, false}));
+    assertEquals(5, aliceAlgorithm(new boolean[]{true, false, false, false, true}));
+    assertEquals(4, aliceAlgorithm(new boolean[]{true, false, false, true, false}));
   }
 
   @Test
@@ -28,6 +30,8 @@ public class Coin1834273354628424080Tests {
     assertEquals(2, bobAlgorithm(new boolean[]{true, true}));
     assertEquals(4, bobAlgorithm(new boolean[]{true, false, false, true}));
     assertEquals(2, bobAlgorithm(new boolean[]{true, false, true, false}));
+    assertEquals(3, bobAlgorithm(new boolean[]{true, false, false, false, true}));
+    assertEquals(5, bobAlgorithm(new boolean[]{true, false, false, true, false}));
   }
 
 }
